ad56b2cc04aa177be4ce48ddba49ed5901ce1c4a
madeleinewoodbury <randine25@gmail.com>
2020-08-18 11:01:08 -0400

    socket io connection in Chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+    socket.emit('join', { name, room }, () => {});
+
+    setName(name);
+    setRoom(room);
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
   return <div>Chat</div>;
 };
 
