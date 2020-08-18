f331f25352a37348ce069cd4452e2709c522c746
madeleinewoodbury <randine25@gmail.com>
2020-08-17 23:03:54 -0400

    Join component and styling

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
-  return <div>Join</div>;
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return (
+    <div className="join outer-container">
+      <div className="inner-container">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            className="input"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className="input mt-20"
+            type="text"
+            placeholder="Room"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="btn mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
